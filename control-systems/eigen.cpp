1e9380da7d6fd4b113063b7fa7625c46b53ec9ea
corvus96 <guillermoraven.96hotmail.com>
2023-10-14 21:57:48 -0400

    Assign matrix entries

--- control-systems/eigen.cpp
+++ control-systems/eigen.cpp
@@ -20,4 +20,19 @@ int main() {
 	//constructor, allocate memory, but do not initialize
 	MatrixXd matrixC(10, 10);
 
+	// assigning matrix entries 
+	MatrixXd matrixC1(2, 2);
+	matrixC1(0, 0) = 1;
+	matrixC1(0, 1) = 2;
+	matrixC1(1, 0) = 3;
+	matrixC1(1, 1) = 4;
+	cout << endl << matrixC1 << endl;
+
+	// fill-in the matrix entries using comma separated values and print the matrix
+	Matrix4f matrixD;
+	matrixD << 1, 2, 3, 4,
+		5, 6, 7, 8,
+		9, 10, 11, 12,
+		13, 14, 15, 16;
+	cout << endl << matrixD << endl;
 }
